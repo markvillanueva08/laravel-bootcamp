8934acd1e024b680e54e0f0c394a9920dd87584a
markvillanueva08 <monkeytheluffy08@mail.com>
2023-09-26 19:57:45 +0800

    laravel-vite

--- laravel-vite/resources/views/index.blade.php
+++ laravel-vite/resources/views/index.blade.php
@@ -9,7 +9,7 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1 class="text-3xl font-bold underlin1">
+    <h1 class="bg-gray-800 mt-2 text-white text-3xl font-bold underline">
         Hello world!
       </h1>
 </body>
